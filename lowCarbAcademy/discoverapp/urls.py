1aeb3673b9daf9eb919314dab3a1662f976510f0
OcularCreo <122617813+OcularCreo@users.noreply.github.com>
2023-10-31 14:52:29 -0400

    added registration, login, logout, search, and database

--- lowCarbAcademy/discoverapp/urls.py
+++ lowCarbAcademy/discoverapp/urls.py
@@ -5,4 +5,7 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('login/', views.login_user, name='login'),
     path('logout/', views.logout_user, name='logout'),
+    path('register/', views.register_user, name='register'),
+    path('search/', views.search_recipes, name='search'),
+    path('show_recipe/<recipe_id>', views.show_recipe, name='show_recipe'),
 ]
--- lowCarbAcademy/discoverapp/views.py
+++ lowCarbAcademy/discoverapp/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from django.contrib.auth.models import User
+from .models import Recipe
+from django.db.models import Q
+
 
 # Create your views here.
 def home(request):
-    return render(request, 'home.html', {})
+    return render(request, 'home.html')
 
 def login_user(request):
 
@@ -23,9 +27,48 @@ def login_user(request):
             return redirect('login')
 
     else:
-        return render(request, 'login.html', {})
+        return render(request, 'login.html')
 
 def logout_user(request):
     logout(request)
     messages.success(request, ("You logged out"))
     return redirect('home')
+
+def register_user(request):
+
+    if request.method == "POST":
+        username = request.POST['username-input']
+        password1 = request.POST['password-input1']
+        password2 = request.POST['password-input2']
+
+        if password1 != password2:
+            print("password not matching - new")
+        elif User.objects.filter(username=username).exists():
+            print("username already taken")
+        else:
+            user = User.objects.create_user(username=username, password=password1)
+            user.save()
+
+            login(request, user)
+
+        return redirect('home')
+
+    else:
+        return render(request, 'register.html')
+
+def search_recipes(request):
+
+    if request.method == "POST":
+        searched = request.POST["search-bar"]
+
+        recipes = Recipe.objects.filter(Q(name__contains=searched) | Q(tags__contains=searched))
+
+        return render(request, 'search.html', {'searched': searched, 'recipes': recipes})
+    else:
+        return render(request, 'search.html')
+
+def show_recipe(request, recipe_id):
+
+    recipe = Recipe.objects.get(pk=recipe_id)
+
+    return render(request, 'recipe.html', {'recipe': recipe})
