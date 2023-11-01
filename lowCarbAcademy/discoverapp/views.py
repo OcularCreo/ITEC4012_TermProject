b26bf83dada8abcc0433f64fcb079cdc81724866
OcularCreo <122617813+OcularCreo@users.noreply.github.com>
2023-11-01 13:07:24 -0400

    working on creating and adding cookbooks

--- lowCarbAcademy/discoverapp/views.py
+++ lowCarbAcademy/discoverapp/views.py
@@ -71,13 +71,18 @@ def search_recipes(request):
     if request.method == "GET":
         searched = request.GET.get("search-bar")
 
-        # using filter to search data base
+        # using filter to search database
         all_recipes = Recipe.objects.filter(Q(name__contains=searched) | Q(tags__contains=searched))
 
         # Pagination
         p = Paginator(all_recipes, 50)
         page = request.GET.get('page')
         recipes = p.get_page(page)
+
+        #find all cookbooks the user might have
+        #user_userRecipe_objs = UserRecipe.objects.filter(user_id=request.user.id)
+        #all_cookbooks = user_userRecipe_objs.values("playlist_name").distinct()
+        #print(all_cookbooks)
 
         return render(request, 'search.html', {'searched': searched, 'recipes': recipes})
     else:
@@ -119,6 +124,32 @@ def favourite_recipe(request):
     return render(request, 'search.html')
 
 def cookbook(request):
+
+    if request.method == "POST":
+
+        # verify that the cookbook doesn't already exist
+
+        # identify current user, get the submitted cookbook name and recipe ID
+        curr_user = request.user.id
+        new_book_name = request.POST["new-book"]
+        recipe_id = request.POST["new-book-recipe"]
+
+        # search for any occurances of the playlist
+        existing_books = UserRecipe.objects.filter(user_id=curr_user, playlist_name=new_book_name)
+
+        if not existing_books:
+
+            # check if user has already saved the recipe as a favourite
+            existing_fav = UserRecipe.objects.filter(user_id=curr_user, recipe_id=recipe_id).latest('user_id', 'recipe_id')
+
+            if not existing_fav:
+                new_book = UserRecipe.objects.create(user_id=request.user.id, recipe_id=recipe_id, favourite=False, playlist_name=new_book_name)
+                new_book.save()
+            else:
+                # just update the playlist name field if recipe is already saved as a favourite
+                existing_fav.update(playlist_name=new_book_name)
+        else:
+            print("book already exists")
 
     # get id of user
 
